00e4d9f8aa9e619f3c96c2760c5deae0d8a68962
Chazal <chazal.hassani@gmail.com>
2022-04-05 10:27:44 +0400

    page login

--- obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -78,7 +78,7 @@ namespace GestionAbsence {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GestionAbsence;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GestionAbsence;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
